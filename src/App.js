5c7fe856c9dbfc360c6bc12400cb73b9c03c94f4
tiagosathler <sathler@gmail.com>
2021-09-13 15:38:03 -0300

    Exercícios 8 e 9: concluídos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import Users from './components/Users'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter >
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">Sobre</Link></li>
-          <li><Link to="/users">Usuários</Link></li>
-        </ul>
-      </nav>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route excat path="/users" component={() => <Users greetingsMessage="Boa tarde" />} />
-      </BrowserRouter>  
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/users">Users</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={ Home }/>
+          <Route exact path="/about" component={ About }/>
+          <Route
+            exact path="/users/:id"
+            render={ (props) => <Users {...props} greetingMessage="Good Morning" /> }
+          />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
-    const { greetingsMessage } = this.props
+    const { 
+      greetingMessage = 'Hi There',
+      match : { params: { id } 
+    } } = this.props
     return (
       <div>
         <h2>Users</h2>
-        <p> { greetingsMessage }, My awesome Users component </p>
+        <p> { `${greetingMessage} User ${id}` }, this  is my awesome Users component </p>
       </div>
     );
   }
