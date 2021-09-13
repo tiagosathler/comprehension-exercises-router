70b27602196972b49226ebc10a1415abce0bd8ca
tiagosathler <sathler@gmail.com>
2021-09-13 15:11:27 -0300

    Exercício 4: concluído; Exercício 2: corrigido

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter >
-        <Route path="/home" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
       </BrowserRouter>  
     );
